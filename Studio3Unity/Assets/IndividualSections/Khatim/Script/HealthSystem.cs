1759ca1c1c364f208be38063904663dd5a924ce9
Khatim <kahtim.dar@gmail.com>
2018-02-12 14:35:23 +0400

    Added the health system

---
+++ Studio3Unity/Assets/IndividualSections/Khatim/Script/HealthSystem.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HealthSystem : MonoBehaviour
+{
+    #region Public Variables
+    public Text healthText;
+    public GameObject healthObj;
+    #endregion
+
+    #region Private Variables
+    private int Score = 0;
+    #endregion
+    // Update is called once per frame
+    void Update ()
+    {
+
+	}
+
+    private void OnTriggerStay(Collider other)
+    {
+        if (other.gameObject.tag == "Health" && Input.GetKey(KeyCode.E))
+        {
+            Score++;
+            healthText.text = Score.ToString();
+            healthObj.SetActive(false);
+        }
+    }
+}
