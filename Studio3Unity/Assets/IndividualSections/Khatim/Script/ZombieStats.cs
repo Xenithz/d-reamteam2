621e10f8d76c88e7bb29c08c1932a688bb5f49c3
Khatim <kahtim.dar@gmail.com>
2018-02-12 12:47:17 +0400

    Added the Player States and Zombie Stats classes. Still working on it. YOUSEF, your computer is sloowwwww

---
+++ Studio3Unity/Assets/IndividualSections/Khatim/Script/ZombieStats.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ZombieStats : MonoBehaviour
+{
+    #region Private Stats Variables
+    private float health = 1.0f;
+    private float regularDamage = 0.5f;
+    private float heavyDamage = 3.0f;
+    #endregion
+
+    #region Private Variables
+    private Animation anim;
+    #endregion
+    #region Public Variables
+
+    #endregion
+    // Use this for initialization
+    void Start ()
+    {
+		
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+		
+	}
+}
