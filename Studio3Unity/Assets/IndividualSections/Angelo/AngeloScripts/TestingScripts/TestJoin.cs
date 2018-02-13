c3e4f57ebd0c88218c44ca4a95d6cb14eb9841db
AngeloG <amcgdev@gmail.com>
2018-02-13 12:39:02 +0400

    my stuff

---
+++ Studio3Unity/Assets/IndividualSections/Angelo/AngeloScripts/TestingScripts/TestJoin.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class TestJoin : MonoBehaviour
+{
+    public bool autoConnect = true;
+    public byte version = 1;
+    public bool connectInUpdate = true;
+
+    public virtual void Start()
+    {
+        PhotonNetwork.autoJoinLobby = false;
+    }
+
+    public virtual void Update()
+    {
+        if (connectInUpdate && autoConnect && !PhotonNetwork.connected)
+        {
+            connectInUpdate = false;
+            PhotonNetwork.ConnectUsingSettings(version + "." + SceneManagerHelper.ActiveSceneBuildIndex);
+        }
+    }
+}
