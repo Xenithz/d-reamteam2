41a86d08db96bbe1ac2f5ad6df970724937a6a98
yousef424 <yousef424ae@yahoo.com>
2018-02-25 00:59:26 -0800

    Camera Controller and Manager
    Made a camera controller, and starting with the tile manager

--- Studio3Unity/Assets/IndividualSections/Koosa/Koosa_Scripts/Camera_Controller.cs
+++ Studio3Unity/Assets/IndividualSections/Koosa/Koosa_Scripts/Camera_Controller.cs
@@ -4,19 +4,25 @@ using UnityEngine;
 
 public class Camera_Controller : MonoBehaviour {
 
-    public float speed;
+
+    public Transform player;
+    public Vector3 target;
     public Vector3 offset;
-    public Camera maincam;
+    public float smoothing;
+    private Vector3 velocity = Vector3.zero;
 
 
 	// Use this for initialization
 	void Start () {
+        
 
 		
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		
-	}
+       target = player.transform.position + offset;
+        transform.position = Vector3.SmoothDamp(transform.position, target, ref velocity, smoothing);
+
+    }
 }
