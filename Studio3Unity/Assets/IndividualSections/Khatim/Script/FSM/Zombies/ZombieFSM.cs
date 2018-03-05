624b2db8a7ec06f9d951a8420dfc156ede600eeb
Khatim <kahtim.dar@gmail.com>
2018-03-05 06:05:42 +0400

    Changed the AI to FSM. Still needs work on the attack state

---
+++ Studio3Unity/Assets/IndividualSections/Khatim/Script/FSM/Zombies/ZombieFSM.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ZombieFSM : MonoBehaviour
+{
+    #region Public Variables
+    #endregion
+
+    #region Private Variables
+    private enum Condition { Chase, Attack };
+    private Condition currCondition;
+    private ZombieStats zom;
+    private Vector3 vel;
+    #endregion
+
+    #region Callbacks
+    // Use this for initialization
+    void Start()
+    {
+        zom = GetComponent<ZombieStats>();
+        zom.rg = GetComponent<Rigidbody>();
+        currCondition = Condition.Chase;
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        zom.distanceToPlayer = Vector3.Distance(transform.position, zom.player.transform.position);
+        if (zom.distanceToPlayer < zom.attackDistance)
+        {
+            currCondition = Condition.Attack;
+        }
+        else
+            currCondition = Condition.Chase;
+
+        switch (currCondition)
+        {
+            case Condition.Chase:
+                transform.LookAt(zom.player.transform.position);
+                zom.speed = Mathf.Clamp(zom.speed, 0, zom.maxSpeed);
+                zom.rg.AddForce(transform.forward * zom.speed);
+                Debug.Log("Chasing");
+                break;
+            case Condition.Attack:
+                Debug.Log("Attacking");
+                break;
+            default:
+                break;
+        }
+    }
+    #endregion
+
+    #region Functions
+
+    #endregion
+
+}
---
+++ Studio3Unity/Assets/IndividualSections/Khatim/Script/Other/ZombieStats.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ZombieStats : MonoBehaviour
+{
+    #region Public Variables
+    public int speed;
+    public int maxSpeed;
+    public int attackDistance;
+    public float distanceToPlayer;
+    [HideInInspector] public Rigidbody rg;
+    public GameObject player;
+    #endregion
+
+    #region Private Variables
+    /*private Animation anim;
+    private int health = 1;
+    private int regularDamage = 1;
+    private int heavyDamage = 3;*/
+    #endregion
+
+    #region Callbacks
+    void FixedUpdate()
+    {
+
+    }
+    #endregion
+
+    #region Functions
+
+    #endregion
+
+    
+}
