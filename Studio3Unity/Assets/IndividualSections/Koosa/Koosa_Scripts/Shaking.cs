7e32fb69a3d75c671901bd82d6268310383c1034
yousef424 <yousef424ae@yahoo.com>
2018-03-06 20:43:10 +0400

    shaking start

---
+++ Studio3Unity/Assets/IndividualSections/Koosa/Koosa_Scripts/Shaking.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Shaking : MonoBehaviour {
+public float delta;
+public float speed;
+public Quaternion startRot;
+public int minSpeed;
+public int maxSpeed;
+	// Use this for initialization
+	void Start () {
+		startRot=transform.rotation;
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		speed=Random.Range(minSpeed,maxSpeed);
+		startRot.z+=delta*Mathf.Sin(speed*Time.deltaTime);
+		transform.rotation=startRot;
+		
+	}
+	
+}
