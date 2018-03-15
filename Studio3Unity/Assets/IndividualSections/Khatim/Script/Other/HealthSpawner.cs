9e63fe50b47f4f8eba4c8d143d9f5ab77246674e
Khatim <kahtim.dar@gmail.com>
2018-03-15 08:30:10 +0400

    Initialized the health spawner scripts

---
+++ Studio3Unity/Assets/IndividualSections/Khatim/Script/Other/HealthSpawner.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HealthSpawner : MonoBehaviour 
+{
+	#region Public Variables
+	public Transform[] healthSpawnLocation;
+	public GameObject healthPrefab;
+	#endregion
+
+	#region Private Variables
+	private int index;
+	public float timer = 0;
+	private float maxTime = 5;
+	#endregion
+
+	#region Callbacks
+	void Start () 
+	{
+		index = Random.Range(0, healthSpawnLocation.Length);
+	}
+	
+	void Update () 
+	{
+		timer -= Time.deltaTime;
+		if (timer <= 0)
+		{
+			//SpawnHealth();
+			Debug.Log("Health Spawned");
+			timer = maxTime;
+		}
+	}
+	#endregion
+
+	#region Functions
+	/*void SpawnHealth()
+	{
+		Instantiate(healthPrefab,,Quaternion.identity);
+	}*/
+	#endregion
+}
