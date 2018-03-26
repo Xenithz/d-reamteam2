fc2aced5041dcd4f3fb6edd0c13083fab82b418c
AngeloG <amcgdev@gmail.com>
2018-03-26 09:51:36 +0400

    123

--- Studio3Unity/Assets/IndividualSections/Angelo/AngeloScripts/ImplementationScripts/GameManagerBase.cs
+++ Studio3Unity/Assets/IndividualSections/Angelo/AngeloScripts/ImplementationScripts/GameManagerBase.cs
@@ -20,16 +20,29 @@ public class GameManagerBase : Photon.PunBehaviour
     public GameObject[] spawnPoints;
 
     public GameObject playerPrefab;
+
+    public int roundNumber;
     #endregion
 
     #region Unity callbacks
     private void Start()
     {
+        roundNumber = 0;
+        myGameState = GameStates.Starting;
         Initialize();
     }
     private void Awake() 
     {
         instance = this;
+    }
+
+    private void Update()
+    {
+        if(AIHandler.instance.aiToTrack.Count == 0)
+        {
+            roundNumber++;
+            SetUpNewRound(roundNumber);
+        }
     }
     #endregion
 
@@ -38,6 +51,7 @@ public class GameManagerBase : Photon.PunBehaviour
     {
         GameObject myPlayer = PhotonNetwork.Instantiate(this.playerPrefab.name, spawnPoints[0].transform.position, Quaternion.identity, 0);
         this.myGameState = GameStates.Starting;
+        SetUpNewRound(roundNumber);
     }
 
     public void UpdateRoundsSurvived()
@@ -45,12 +59,12 @@ public class GameManagerBase : Photon.PunBehaviour
         UserInformationControl.instance.CallEditData(UserStats.instance.myUsername);
     }
 
-    public void SetUpNewRound()
+    public void SetUpNewRound(int roundNumberToPass)
     {
-        
+        Zombie_Pool.zombiePoolInstance.Spawn(roundNumberToPass);
     }
     
-    public void EndGame ()
+    public void EndGame()
     {
 
     }
