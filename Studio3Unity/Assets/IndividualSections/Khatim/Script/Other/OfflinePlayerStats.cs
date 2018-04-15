f9382ddb79dccee96dabe0b9f8f7f15a163944b9
Khatim <kahtim.dar@gmail.com>
2018-04-15 21:05:02 +0400

    Improved the Flee behaviour, made most of my mechanics work offline now. The seeking behaviour needs to be improved. And gonna integrate it in the offline scene

---
+++ Studio3Unity/Assets/IndividualSections/Khatim/Script/Other/OfflinePlayerStats.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OfflinePlayerStats : MonoBehaviour
+{
+    #region Public Variables
+    public GameObject[] healthsprites;
+    public GameObject player;
+    public int health = 6;
+    #endregion
+
+    #region Private Variables
+    #endregion
+
+    #region Callbacks
+    void Awake()
+    {
+        player = GameObject.FindGameObjectWithTag("Player");
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.R))
+        {
+            Damage();
+        }
+
+        if (health <= 0)
+        {
+            player.SetActive(false);
+        }
+    }
+    #endregion
+
+    #region Functions
+    public void Damage()
+    {
+        health--;
+        healthsprites[health].SetActive(false);
+    }
+
+    public void Heal()
+    {
+        healthsprites[health].SetActive(true);
+        health++;
+    }
+    #endregion
+}
