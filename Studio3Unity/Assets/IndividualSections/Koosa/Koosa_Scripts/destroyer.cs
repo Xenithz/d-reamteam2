5ebd47fd6e52bd1b862b005c68135bf517d7699f
yousef424 <yousef424ae@yahoo.com>
2018-04-16 11:50:33 +0400

    213123123123213123

--- Studio3Unity/Assets/IndividualSections/Koosa/Koosa_Scripts/destroyer.cs
+++ Studio3Unity/Assets/IndividualSections/Koosa/Koosa_Scripts/destroyer.cs
@@ -2,7 +2,9 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class destroyer : MonoBehaviour {
+public class destroyer : MonoBehaviour 
+{
+	public TutorialHandler tutorial;
 	void OnCollisionEnter(Collision other)
 	{
         if (other.gameObject.tag == "zombie1")
