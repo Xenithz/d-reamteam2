ca0d0fac685a7eb94bf479025944b04bdce93ed9
yousef424 <yousef424ae@yahoo.com>
2018-04-22 08:35:31 +0400

    things

--- Studio3Unity/Assets/IndividualSections/Koosa/Koosa_Offline_stuff/OfflineCameraController.cs
+++ Studio3Unity/Assets/IndividualSections/Koosa/Koosa_Offline_stuff/OfflineCameraController.cs
@@ -4,52 +4,64 @@ using UnityEngine;
 
 public class OfflineCameraController : MonoBehaviour 
 {
-	public List <Transform> Players;
+	#region Public Variables
 	public Vector3 offset;
-	public Vector3 vel;
+	public Vector3 velocity;
 	public float smoothing;
 	public float minZoom;
 	public float maxZoom;
 	public float limitZoom;
 	public Camera playerCam;
-void Start()
+	#endregion
+	#region Private Variables
+	private Bounds bounds;
+	private List <Transform> Players;
+	#endregion
+    #region Unity Functions
+void Awake()
 {
 	playerCam=GetComponent<Camera>();
-	
+	foreach (GameObject player in GameObject.FindGameObjectsWithTag("Player"))
+        {
+            Players.Add(player.transform);
+        }
 }
 	void LateUpdate()
 	{
 		if(Players.Count==0)
 		return;
-		MoveCamera();
+		CameraMove();
 	}
-	public void ZoomCamera()
+	#endregion
+	#region My Functions
+	public void ZoomCameraIn()
 	{
-		float updatedZoom=Mathf.Lerp(maxZoom,minZoom,GetDistance()/limitZoom);
+		float updatedZoom=Mathf.Lerp(maxZoom,minZoom,Distance()/limitZoom);
         playerCam.fieldOfView=Mathf.Lerp(playerCam.fieldOfView,updatedZoom ,Time.deltaTime);
 	}
-	public void MoveCamera()
+	public void CameraMove()
 	{
-		Vector3 focusPoint=GetFocusPoint();
+		Vector3 focusPoint=PointToFocus();
 		Vector3 updatedPosition=focusPoint+offset;
-		transform.position=Vector3.SmoothDamp(transform.position,updatedPosition,ref vel, smoothing*Time.deltaTime);
+		transform.position=Vector3.SmoothDamp(transform.position,updatedPosition,ref velocity, smoothing*Time.deltaTime);
 	}
-	Vector3 GetFocusPoint()
+	Vector3 PointToFocus()
 	{
 		if(Players.Count==1)
 		{
         return Players[0].position;
 		}
-		var bounds =new Bounds(Players[0].position,Vector3.zero);
+		bounds =new Bounds(Players[0].position,Vector3.zero);
+
 		for (int i = 0; i < Players.Count; i++)
 		{
 		bounds.Encapsulate(Players[i].position);
 		}
 		return bounds.center;
 	}
-	float GetDistance()
+	float Distance()
 	{
-		var bounds= new Bounds (Players[0].position,Vector3.zero);
+	    bounds= new Bounds (Players[0].position,Vector3.zero);
 		for (int i = 0; i < Players.Count; i++)
 		{
 		bounds.Encapsulate(Players[i].position);
@@ -57,3 +69,4 @@ void Start()
 		return bounds.size.x;
 	}
 }
+#endregion
