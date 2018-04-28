87a7a8919aac5940070f98ea8baffabdfd14bb33
Khatim <kahtim.dar@gmail.com>
2018-04-28 14:25:25 +0400

    Just working on a level editor.

---
+++ Studio3Unity/Assets/IndividualSections/Khatim/Script/Level Editor/ColourToPrefab.cs
@@ -0,0 +1,9 @@
+﻿
+using UnityEngine;
+
+[System.Serializable]
+public class ColourToPrefab
+{
+    public Color colour;
+    public GameObject prefabObj;
+}
---
+++ Studio3Unity/Assets/IndividualSections/Khatim/Script/Level Editor/LevelGenerator.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+
+public class LevelGenerator : MonoBehaviour
+{
+    public Texture2D level;
+    public ColourToPrefab[] colourMap;
+    // Use this for initialization
+    void Start()
+    {
+        LevelGenerate();
+    }
+
+    void LevelGenerate()
+    {
+        for (int x = 0; x < level.width; x++)
+        {
+            for (int z = 0; z < level.height; z++)
+            {
+                TileGenerate(x, z);
+            }
+        }
+    }
+
+    void TileGenerate(int x, int z)
+    {
+        Color pixel = level.GetPixel(x, z);
+
+        if (pixel.a == 0)
+        {
+            return;
+        }
+
+        foreach (ColourToPrefab mapColours in colourMap)
+        {
+            if (mapColours.colour.Equals(pixel))
+            {
+                Vector3 pos = new Vector3(x, 0, z);
+                Instantiate(mapColours.prefabObj, pos, Quaternion.Euler(-90, 0, 0));
+            }
+        }
+    }
+}
